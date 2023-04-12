4d92ab6e0eb125247f4378fc8bb2521c07cebc9b
Riley Flynn <riley@rileyflynn.me>
2023-04-12 20:38:28 -0230

    Add missing file, fix default bind addr

---
+++ pkg/server/proxies/go/proxy.go
@@ -0,0 +1,49 @@
+package goproxy
+
+import (
+	_ "embed"
+	"html/template"
+	"net/http"
+
+	"golang.org/x/exp/slog"
+
+	"github.com/nint8835/dieppe/pkg/config"
+)
+
+type proxyRespCtx struct {
+	ImportPath  string
+	VCSType     string
+	UpstreamURL string
+}
+
+//go:embed proxy_resp.gohtml
+var respTmplBody string
+
+var respTmpl = template.Must(template.New("proxy_resp").Parse(respTmplBody))
+
+type GoProxy struct {
+	config *config.Config
+	module *config.GoModule
+}
+
+func (p *GoProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_ = respTmpl.Execute(w, proxyRespCtx{
+		ImportPath:  p.module.ImportPath(&p.config.Server),
+		VCSType:     "git",
+		UpstreamURL: p.module.Upstream,
+	})
+}
+
+func Register(cfg *config.Config, mux *http.ServeMux) {
+	for _, module := range cfg.GoModules {
+		proxy := &GoProxy{
+			config: cfg,
+			module: &module,
+		}
+
+		proxyPath := "/" + module.Path
+		mux.Handle(proxyPath, proxy)
+
+		slog.Debug("Registered Go module proxy", "module", module.ID, "path", proxyPath)
+	}
+}
